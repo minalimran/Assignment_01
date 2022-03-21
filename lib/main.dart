da609e3d5fe7f2e16f192492625deda25b479d09
minalimran <rimshakazmi693@gmail.com>
2022-03-21 06:55:49 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.red,
+          title: Center(child:Text('first assignment') ,),
+          actions: [
+            Icon(Icons.access_alarm)
+          ],
+
+        ),
+
+        body: Container(
+          color: Colors.black,
+        ),
+      )
+      
+    );
+  }
+}
